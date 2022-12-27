59fb2e3480a8ed92761ed2ea7594dfa48a34142b
Amanda Guerriero <ag.codingclass@gmail.com>
2022-12-26 22:37:37 -0800

    Style Changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
+import './style.css';
 
-function Nav() {
+const Nav = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <div>
+      <div className="navbar">
+        <p></p>
+        <div className="navbar-right">
+          <a href="/">HOME</a>
+          <NavLink 
+            to="/story"
+            activeClassName="active"
+            >OUR STORY</NavLink>
+        </div>
+      </div>
+      <div className="header-container">
+        <a href="/" id="background"/>
+        {/* <a href="/register-for-updates"><button className="reg-button">Get Updates</button></a> */}
+      </div>
+    </div>
   );
 }
 
